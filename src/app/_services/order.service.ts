9cb145582b8f43a1b7cc51fb81eb09aa3bcd0f14
Your Name <you@example.com>
2021-10-20 20:57:19 +0530

    order history working now

--- src/app/_services/order.service.ts
+++ src/app/_services/order.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 //import { User } from '@/_models';
 import { config } from 'process';
 import { Order } from '../product-order/order';
@@ -11,7 +11,9 @@ export class OrderService {
     order(orderInfo: Order) {
       //console.log("service called")
       //console.log(JSON.stringify(orderInfo));
-        return this.http.post(this.apiUrl, orderInfo);
+      let header = new HttpHeaders({ "Authorization": "Bearer "+localStorage.getItem('token'),"Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers":"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With"});
+    const requestOptions = {  headers: header};
+        return this.http.post(this.apiUrl, orderInfo ,requestOptions);
     }
 
 }
